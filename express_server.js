b59bac21515beb25b445b4cbcac9de4a1cd26681
Upgrayedd0408 <michaellowry89@gmail.com>
2024-01-05 20:54:04 +0000

    added a feature to display the username in the nav bar

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080;
+const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
 
@@ -10,7 +11,7 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -19,17 +20,24 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase,
-
+    username: req.cookies["username"]
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+   };
   res.render("urls_show", templateVars);
 });
 
