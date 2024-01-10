30a2f7e99ada5970466cf035038b29d38a8aae8d
Upgrayedd0408 <michaellowry89@gmail.com>
2024-01-10 20:13:21 +0000

    added a register new user field

--- express_server.js
+++ express_server.js
@@ -54,6 +54,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body</html>\n");
 });
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.post("/urls", (req, res) => {
   // console.log(req.body); // Log the POST request body to the console
   // res.send("Ok"); // Respond with 'Ok' (we will replace this)
