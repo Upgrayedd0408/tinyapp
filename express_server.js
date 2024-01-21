5e7d3e277c211884d451d438384a4dea12703906
Upgrayedd0408 <michaellowry89@gmail.com>
2024-01-21 17:19:00 +0000

    deleted commented out users and urlDatabase code

--- express_server.js
+++ express_server.js
@@ -15,38 +15,11 @@ app.use(cookieSession({
   keys: ['randomkey1', 'randomkey2']
 }));
 
-// const urlDatabase = {
-//   "b2xVn2": {
-//     longURL: "http://www.lighthouselabs.ca",
-//     userID: "aJ48lW",
-//   },
-//   "9sm5xK": {
-//     longURL: "http://www.google.com",
-//     userID: "aJ48lW",
-//   },
-// };
 
 // set's how many salt rounds being used during hashing.
 const salt = bcrypt.genSaltSync(10);
 
 
-// const users = {
-//   userRandomID: {
-//     id: 'userRandomID',
-//     email: 'user@example.com',
-//     password: bcrypt.hashSync('purple-monkey-dinosaur', salt)
-//   },
-//   user2RandomID: {
-//     id: 'user2RandomID',
-//     email: 'user2@example.com',
-//     password: bcrypt.hashSync('dishwasher-funk', salt)
-//   },
-//   aJ48lW: {
-//     id: 'aJ48lW',
-//     email: 'user3@example.com',
-//     password: bcrypt.hashSync('123', salt)
-//   }
-// };
 
 
 app.get("/", (req, res) => {
@@ -229,7 +202,7 @@ app.post("/register", (req, res) => {
   //assign user inputs to variables
   const email = req.body.email;
   const password = req.body.password;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
   
   // Display error message if user leaves fields blank
   if (!email || !password) {
